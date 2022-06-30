ece6ed6bd3781befd0f5d62661e15dcb8eaa5aa4
Pradnya Savakhande <pradnya@dhitechnologies.org>
2022-06-30 09:11:26 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user/user.component';
+import { VolunteerComponent } from './volunteer/volunteer/volunteer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'user', component : UserComponent},
+  {path: 'volunteer', component: VolunteerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user/user.component';
+import { VolunteerComponent } from './volunteer/volunteer/volunteer.component';
+import { EventComponent } from './event/event/event.component';
+import { ServiceProviderComponent } from './service-provider/service-provider/service-provider.component';
+import { LoginComponent } from './core/auth/pages/login/login.component';
+import { RegistrationComponent } from './core/auth/pages/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    VolunteerComponent,
+    EventComponent,
+    ServiceProviderComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
